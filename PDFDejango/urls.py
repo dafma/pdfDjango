e3b6e5c13a72df0c04b173f019e3016a50ffa981
dafma <mar_tp@hotmail.es>
2017-04-30 20:07:01 -0500

    Code

--- PDFDejango/urls.py
+++ PDFDejango/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from app.views import  GeneratePDF
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^pdf/', GeneratePDF.as_view()),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
+from django.template.loader import get_template
 
-# Create your views here.
+from .utils import render_to_pdf #created in step 4
+
+class GeneratePDF(View):
+    def get(self, request, *args, **kwargs):
+        template = 'pdf/invoice.html'
+        context = {
+            "invoice_id": 123,
+            "customer_name": "John Cooper",
+            "amount": 1399.99,
+            "today": "Today",
+        }
+        pdf = render_to_pdf('invoice.html', context)
+        if pdf:
+            response = HttpResponse(pdf, content_type='application/pdf')
+            filename = "Invoice_%s.pdf" % ("12341231")
+            content = "inline; filename='%s'" % (filename)
+            download = request.GET.get("download")
+            if download:
+                content = "attachment; filename='%s'" % (filename)
+            response['Content-Disposition'] = content
+            return response
+        return HttpResponse("Not found")
